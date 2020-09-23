76145f77c92d414b690d87df1c52c51adf2205e3
vijay prakash <vijayprakash2829@outlook.com>
2020-09-23 09:12:41 +0530

    Added XML Comments

--- CartPromotionEngine/Program.cs
+++ CartPromotionEngine/Program.cs
@@ -7,14 +7,21 @@ using System.Text.RegularExpressions;
 
 namespace CartPromotionEngine
 {
+    //Class to calculate Promotions for SKU
     public static class Program
     {
+        //Value of Unit A
         public static int A = 50;
+        //Value of Unit B
         public static int B = 30;
+        //Value of Unit C
         public static int C = 20;
+        //Value of Unit D
         public static int D = 15;
+        //Value for Promotions Calculation
         public static int PromotionsCalculation;
 
+        //Method to define and calculate Promotions
         public static int Promotions(string UnitName, int UnitsRequired)
         {
             int remainder;
@@ -41,18 +48,23 @@ namespace CartPromotionEngine
             return PromotionsCalculation;
         }
 
+        //Method to return the amount of one Promotion
         public static int Calculate(string UnitName, int UnitsRequired)
         {
             int TotalAmount = Promotions(UnitName.ToLower(), UnitsRequired);
             return TotalAmount;
         }
 
+        //Main method to get user input and returns the total amount in command window
         public static void Main(string[] args)
         {
+            //Value of Output, intially it was 0
             int Output = 0;
+            //Dictionary to story user inputs
             Dictionary<string, int> dict = new Dictionary<string, int>();
             Console.WriteLine("Press Enter key to Continue:");
             string Entry = Console.ReadLine();
+            //To quit getting User input, press "q" and enter
             while (Entry != "q")
             {
                 Console.WriteLine("Enter Unit:");
@@ -65,6 +77,7 @@ namespace CartPromotionEngine
                 int UnitReq = 0;
                 if ((Unit != "") || (Unit.GetType().Equals(typeof(string))))
                 {
+                    //Regex to get the match the units available
                     Match m = Regex.Match(Unit, @"[a-dA-D]");
                     if (m.Success)
                     {
@@ -72,11 +85,13 @@ namespace CartPromotionEngine
                         UnitReq = Convert.ToInt32(Console.ReadLine());
                         dict.Add(Unit.ToLower(), UnitReq);
                     }
+                    //Writes to user, that no units is available
                     else
                     {
                         Console.WriteLine("The Unit is not available");
                     }
                 }
+                //Writes to user, that no units is available
                 else
                 {
                     Console.WriteLine("The Unit is not available");
@@ -84,6 +99,7 @@ namespace CartPromotionEngine
                 Console.WriteLine("Press Enter key to Continue or Type 'q' to exit : ");
                 Entry = Console.ReadLine();
             }
+            //Condition to consolidate all the input Units if it contains units both C & D to give SKUs
             if ((dict.ContainsKey("c") && dict.ContainsKey("d")))
             {
                 foreach (var dic in dict)
@@ -111,6 +127,7 @@ namespace CartPromotionEngine
                     Output += res;
                 }
             }
+            //Processed Output
             Console.WriteLine("Total amount of Units purchased: {0} ", Output);
             Console.ReadLine();
         }
