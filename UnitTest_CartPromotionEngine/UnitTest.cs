c60437cf64123d1431b72b4319a7dde10d858974
vijay prakash <vijayprakash2829@outlook.com>
2020-09-23 09:21:22 +0530

    Unit test methods for Scenario A,B & C

--- UnitTest_CartPromotionEngine/UnitTest.cs
+++ UnitTest_CartPromotionEngine/UnitTest.cs
@@ -8,7 +8,6 @@ namespace UnitTest_CartPromotionEngine
     [TestClass]
     public class UnitTest
     {
-        //Unit Test Methods to test Scenario A
         [TestMethod]
         public void ScenarioA()
         {
@@ -22,6 +21,64 @@ namespace UnitTest_CartPromotionEngine
             {
                 var Output = Program.Calculate(dic.Key, dic.Value);
                 actualOutput += Output;
+            }
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void ScenarioB()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("A", 5);
+            dict.Add("B", 5);
+            dict.Add("C", 1);
+            int expectedOutput = 370;
+            int actualOutput = 0;
+            foreach (var dic in dict)
+            {
+                var Output = Program.Calculate(dic.Key, dic.Value);
+                actualOutput += Output;
+            }
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void ScenarioC()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("A", 3);
+            dict.Add("B", 5);
+            dict.Add("C", 1);
+            dict.Add("D", 1);
+
+            int expectedOutput = 280;
+            int actualOutput = 0;
+            if (dict.ContainsKey("C") && dict.ContainsKey("D"))
+            {
+                foreach (var dic in dict)
+                {
+                    if ((dic.Key == "C"))
+                    {
+                        actualOutput += 0;
+                    }
+                    else if (dic.Key == "D")
+                    {
+                        actualOutput += 30;
+                    }
+                    else
+                    {
+                        int Output = Program.Calculate(dic.Key, dic.Value);
+                        actualOutput += Output;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var dic in dict)
+                {
+                    int Output = Program.Calculate(dic.Key, dic.Value);
+                    actualOutput += Output;
+                }
             }
             Assert.AreEqual(expectedOutput, actualOutput);
         }
