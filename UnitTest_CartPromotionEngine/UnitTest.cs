f8f66e83f8dedeef9082e70b1dc8a9fb05b2a0b7
vijay prakash <vijayprakash2829@outlook.com>
2020-09-23 09:18:39 +0530

    Added project reference for Unit tests with a sample test

--- UnitTest_CartPromotionEngine/UnitTest.cs
+++ UnitTest_CartPromotionEngine/UnitTest.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CartPromotionEngine;
+using System.Collections.Generic;
 
 namespace UnitTest_CartPromotionEngine
 {
     [TestClass]
     public class UnitTest
     {
+        //Unit Test Methods to test Scenario A
         [TestMethod]
-        public void TestMethod1()
+        public void ScenarioA()
         {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("A", 1);
+            dict.Add("B", 1);
+            dict.Add("C", 1);
+            int expectedOutput = 100;
+            int actualOutput = 0;
+            foreach (var dic in dict)
+            {
+                var Output = Program.Calculate(dic.Key, dic.Value);
+                actualOutput += Output;
+            }
+            Assert.AreEqual(expectedOutput, actualOutput);
         }
     }
 }
